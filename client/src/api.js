c5d3b591d8957572e60011165784fd2d3d0c0047
AKASH-18 <akashakashbisht@gmail.com>
2026-04-13 13:05:21 +0530

    server link changed

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, //"http://localhost:5000/api" import.meta.env.VITE_API_URL
+  baseURL: "https://crm-project-1-2o2n.onrender.com/api", //"http://localhost:5000/api" import.meta.env.VITE_API_URL
 });
 
 export default API;
